4c01bb66896a8b3a411c91173a0289909c492ba0
Trevör Anne Denise <trevor.annedenise@icloud.com>
2020-01-05 18:19:33 +0100

    Revert "Revert "Merge branch 'master' into pooling""
    This reverts commit e9ccc4b0d8d8a29ba5c4ce7e163a7366523dddf8.

--- Sources/AsyncHTTPClient/Utils.swift
+++ Sources/AsyncHTTPClient/Utils.swift
@@ -16,6 +16,18 @@ import NIO
 import NIOHTTP1
 import NIOHTTPCompression
 
+internal extension String {
+    var isIPAddress: Bool {
+        var ipv4Addr = in_addr()
+        var ipv6Addr = in6_addr()
+
+        return self.withCString { ptr in
+            inet_pton(AF_INET, ptr, &ipv4Addr) == 1 ||
+                inet_pton(AF_INET6, ptr, &ipv6Addr) == 1
+        }
+    }
+}
+
 public final class HTTPClientCopyingDelegate: HTTPClientResponseDelegate {
     public typealias Response = Void
 
